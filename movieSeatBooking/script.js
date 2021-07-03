ec8d7007ed1441c4b9a40dbeec506b0b4fdd4183
samson <samsonojugo@gmail.com>
2021-07-03 01:47:38 +0100

    complete project

--- movieSeatBooking/script.js
+++ movieSeatBooking/script.js
@@ -2,18 +2,40 @@
 const container =document.querySelector('.container');
 //using querySelectorAll becos we have many class with the name seat
 //And grabbing all the seat in a row that are not occupied 
-const seats = document.querySelectorAll('.row.seat:not(.occupied)');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 
 const count=document.getElementById('count');
 const total=document.getElementById('total');
 const movieSelect=document.getElementById('movie');
 
+populateUi();
+
 let ticketPrice=+movieSelect.value;// changing d datatype to number
+
+
+
+//function for saving movieIdex and price
+
+function setMovieData(movieIndex, moviePrice){
+	localStorage.setItem('selectedMovieIndex', movieIndex);
+	localStorage.setItem('selectedMoviePrice', moviePrice);
+
+}
 
 //update total and count
 function updateSelectedCount(){
 	//getting all seat with class name selected
 	const selectedSeats=document.querySelectorAll('.row .seat.selected');
+	
+
+	//Copy selected seats into array
+	//Map through array
+	//return a new array indexes
+	const seatsIndex=[...selectedSeats].map((seat)=> [...seats].indexOf(seat) 
+	);
+	
+	localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex))//JSON.stringify was use to turn the array into a string
+
 	//get the lenght of a node
 	const selectedSeatsCount=selectedSeats.length;
 
@@ -21,9 +43,34 @@ function updateSelectedCount(){
 	total.innerText= selectedSeatsCount * ticketPrice;
 }
 
+//data from localStorage and populate UI
+
+function populateUi(){
+	const selectedSeats =JSON.parse(localStorage.getItem('selectedSeats'))//converting it back to array with JSON.parse
+
+	if (selectedSeats !==null && selectedSeats.length >0) {
+
+		seats.forEach((seat, index)=> {
+			if (selectedSeats.indexOf(index)> -1) {
+
+				seat.classList.add('selected');
+			}
+		});
+	}
+
+	const selectedMovieIndex=localStorage.getItem('selectedMovieIndex');
+
+	if (selectedMovieIndex !==null) {
+
+		movieSelect.selectedIndex=selectedMovieIndex;
+	}
+}
+
+
 //movie select event 
 movieSelect.addEventListener('change', e=>{
 	ticketPrice=+e.target.value;
+	setMovieData(e.target.selectedIndex, e.target.value);
 	updateSelectedCount();
 })
 //end
@@ -42,3 +89,5 @@ container.addEventListener('click', e=>{
 		updateSelectedCount();
 	}
 })
+
+updateSelectedCount();
---
+++ test.js
@@ -0,0 +1,5 @@
+const arr=[1,2,3];
+
+const arr2=[...arr,4,5];
+
+console.log(arr2)
